85bead9f5e7cd2467a9e1ef105821dcf8a20884e
Sidney Schwartz <60203408+sidneyschwartz@users.noreply.github.com>
2022-11-18 16:50:58 -0500

    more setup

--- src/App.js
+++ src/App.js
@@ -1,27 +1,44 @@
 import logo from './logo.svg';
 import './App.css';
 import { useState } from 'react';
+import PlayerItem from "./components/PlayerItem";
+import Navigation from "./components/NavBar";
+import 'bootstrap/dist/css/bootstrap.min.css';
+import playerData from "./assets/player-data.json";
+
+
 
 function App() {
 
   const [type, setType] = useState("All");
+  const [cart, setCart] = useState({}); // initialize an empty map
+  const [list, setList] = useState([]); // initialize an empty array
+
+  const selectFilterType = eventKey => {
+    setType(eventKey);
+};
+
+  const matchesFilterType = item => {
+    // all items should be shown when no filter is selected
+    if(type === "All") {
+        return true
+    } else if (type === item.type) {
+        return true
+    } else {
+        return false
+    }
+  }
+
+  // TODO: Write filter function
 
   return (
     <div className="App">
+      <Navigation onSelect={selectFilterType}></Navigation>
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
       </header>
+      <body>
+
+      </body>
     </div>
   );
 }
